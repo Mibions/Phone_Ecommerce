56523b425d2637c2ca2140769563f2c2ad37e5c8
Mibions <nlbthanh@gmail.com>
2024-05-08 16:38:18 +0700

    add existing project

---
+++ Demo_WebAPI/Demo_WebAPI/Controllers/ThongTinNhomController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Demo_WebAPI.Models;
+
+namespace Demo_WebAPI.Controllers
+{
+    public class ThongTinNhomController : ApiController
+    {
+        [HttpGet]
+        [Route("")]
+        public IEnumerable<ThanhVienNhom> Get()
+        {
+            string nhiemVuHao = "";
+            string nhiemVuVuong = "";
+            string nhiemVuVinh = "";
+            string nhiemVuThanh = "";
+            string nhiemVuQuynh = "";
+            ThanhVienNhom tv1 = new ThanhVienNhom("", "Hào", nhiemVuHao, "100%");
+            ThanhVienNhom tv2 = new ThanhVienNhom("", "Quỳnh", nhiemVuVuong, "100%");
+            ThanhVienNhom tv3 = new ThanhVienNhom("", "Vĩnh", nhiemVuVinh, "100%");
+            ThanhVienNhom tv4 = new ThanhVienNhom("", "Thành", nhiemVuThanh, "100%");
+            ThanhVienNhom tv5 = new ThanhVienNhom("", "Vương", nhiemVuQuynh, "100%");
+            List<ThanhVienNhom> listThanhVien = new List<ThanhVienNhom>();
+            listThanhVien.Add(tv1);
+            listThanhVien.Add(tv2);
+            listThanhVien.Add(tv3);
+            listThanhVien.Add(tv4);
+            listThanhVien.Add(tv5);
+            return listThanhVien.ToList();
+        }
+    }
+}
---
+++ Demo_WebAPI/Demo_WebAPI/Models/ThanhVienNhom.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Demo_WebAPI.Models
+{
+    public class ThanhVienNhom
+    {
+        public string MSSV { get; set; }
+        public string hoTen { get; set; }
+        public string nhiemVu { get; set; }
+        public string danhGia { get; set; }
+        public ThanhVienNhom()
+        {
+
+        }
+        public ThanhVienNhom(string mssv, string hoten, string nhiemvu, string danhgia)
+        {
+            this.MSSV = mssv;
+            this.hoTen = hoten;
+            this.nhiemVu = nhiemvu;
+            this.danhGia = danhgia;
+        }
+    }
+}
